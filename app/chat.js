99d2c3afa74ceb3931d451b65f3e6245c9612931
git <igerych19@gmail.com>
2021-07-14 11:55:52 +0300

    initial commit

---
+++ app/chat.js
@@ -0,0 +1,109 @@
+
+// Create Socket
+var message;
+var temp;
+var messages=[];
+var json;
+var name = prompt('введите имя' )
+const socket = new WebSocket('ws://localhost:8081');
+// обработчик проинформирует в консоль когда соединение установится
+socket.onopen = function () {
+  console.log('connect Server');
+};
+// обработчик сообщений от сервера
+socket.onmessage = function (messageFromServer) {
+
+  console.log('Message: %s', messageFromServer.data)
+    // console.log(messageFromServer);
+  if (json == messageFromServer.data) {
+    console.log('equal data');
+    return
+  } else {
+    console.log('not equal');
+    print(messageFromServer.data);
+  }
+};
+// Обработчик ошибок
+socket.onerror = function (error) {
+  console.log('onerror: '+ error.message);
+};
+// функция для отправки echo-сообщений на сервер
+function wsSendEcho(value) {
+  socket.send(JSON.stringify({action: 'ECHO', data: value.toString()}));
+}
+// функция для отправки команды ping на сервер
+function wsSendPing() {
+  socket.send(JSON.stringify({action: 'PING'}));
+}
+
+
+var input=document.getElementById("input");
+var button=document.getElementsByClassName("button")[0];
+button.addEventListener('click',function(event){
+event.preventDefault();
+});
+input.addEventListener("keypress",function(event){
+ if(event.key=="Enter"){send();render();
+ event.preventDefault()}}
+);
+var flag=false;
+
+function print(messageFromServer) {
+messages.push(JSON.parse(messageFromServer));
+flag = 1;
+messages.forEach((item,i) => {
+message=item.message;
+temp=item.date+" "+"("+item.name+")";
+});
+    render();
+    console.log(messages);
+}
+
+function send(){
+  flag = 0;
+date=new Date();
+var time= " "+date.getHours()+" : "+date.getMinutes()+" ";
+messages.push({'name':name,  'date': time, 'message': input.value});
+console.log(messages);
+// Send to Server
+ messages.forEach((item,i) => {
+  message=item.message;
+  temp=item.date+" "+"("+item.name+")";
+  });
+  json = JSON.stringify({'name':name,  'date': time, 'message': input.value})
+  socket.send(json);
+  input.value="";
+ };
+
+render=()=>{
+if (message!=""){
+  var ul =document.createElement("ul");
+  var li=document.createElement("li");
+  var span = document.createElement("span");
+  var span1 = document.createElement("span");
+  span.innerText=message+"  ";
+  span1.innerText=temp;
+  var rotationMes = document.createElement("div");
+  var tail = document.createElement("div");
+  tail.classList.add("tail");
+  rotationMes.appendChild(span);
+  rotationMes.appendChild(span1);
+  rotationMes.appendChild(tail);
+  li.appendChild(rotationMes);
+  span1.classList.add("dat");
+    if (flag==0){
+    rotationMes.classList.add("chat");
+    tail.classList.add("tail1");
+    li.classList.add ('li1')}
+    else {
+    rotationMes.classList.add("chat2");
+    tail.classList.add("tail2")
+    li.classList.add ('li2');
+    }
+  ul.appendChild(li);
+  var div=document.getElementsByClassName("chat-msgs")[0];
+
+  div.appendChild(ul);
+  div.scrollBy(100,100);
+}
+};
---
+++ app/index.js
@@ -0,0 +1,47 @@
+// app/index.js
+const http = require('http');
+const fs = require("fs");
+const WebSocket = require('ws');
+const hostname = '127.0.0.1';
+const port = 8080;
+var clients = {};
+var id = 0;
+var server = http.createServer(function(req, res){
+  filePath='app'+req.url;
+    fs.readFile(filePath,function (err, data){
+
+      if (err) {
+        console.log(err);
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end();
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        console.log("Url: " + req.url);
+       // console.log(req.headers);
+        res.end();
+      }
+    });
+}).listen(port);
+function echoToAll (mes) {
+  Object.keys(clients).forEach((item, i) => {
+      clients[item].send(mes);
+  });
+}
+const wsServer = new WebSocket.Server({port:8081});
+wsServer.on('connection', onConnect);
+function onConnect(wsClient){
+let number = id;
+id +=1;
+clients[number] = wsClient;
+console.log('Новый клиент : [id] ' +number + ' Всего клиентов : '+ Object.keys(clients).length);
+
+// wsClient.send('Новый клиент [id]' +Object.keys(clients));
+wsClient.on('message',function(message){echoToAll(message);
+console.log(message);
+})
+wsClient.on('close',function(){console.log('Клиент : [id] ' +number +' вышел');
+delete clients[number];
+console.log('--> Всего клиентов : '+ Object.keys(clients).length);
+
+})};
